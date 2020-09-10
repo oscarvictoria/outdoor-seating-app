36c4e3d82c49ea366b53a6fe13b904d8a28712ef
oscar victoria gonzalez <oscarvictoriagonzalez@gmail.com>
2020-09-10 17:09:57 -0400

    added xcode project

---
+++ Outdoor-Seating-App/Outdoor-Seating-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Outdoor-Seating-App
+//
+//  Created by Oscar Victoria Gonzalez  on 9/10/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Outdoor-Seating-App/Pods/Target Support Files/Pods-Outdoor-Seating-App/Pods-Outdoor-Seating-App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Outdoor_Seating_App : NSObject
+@end
+@implementation PodsDummy_Pods_Outdoor_Seating_App
+@end
